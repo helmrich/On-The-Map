1cc4a96d6341b77afb8892588465938b0e44df8c
Tobias Helmrich <me@tobias-helmrich.de>
2016-10-14 19:03:00 +0200

    Add FBSDK delegate application method

--- On The Map/AppDelegate.swift
+++ On The Map/AppDelegate.swift
@@ -13,10 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, options: options)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
