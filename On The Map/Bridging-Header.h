4496018f395917dfe782c32ef9dbf6c768bbe0ea
Tobias Helmrich <me@tobias-helmrich.de>
2016-10-14 19:05:19 +0200

    Add a bridging-header file in order to use the FBSDK

---
+++ On The Map/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  On The Map
+//
+//  Created by Tobias Helmrich on 13.10.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Bridging_Header_h */
