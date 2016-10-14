ddae53044997b0c3a9891bd50efddff8dde5b997
Tobias Helmrich <me@tobias-helmrich.de>
2016-10-14 19:36:36 +0200

    Add enum for errors specific to the Udacity client

---
+++ On The Map/UdacityClientError.swift
@@ -0,0 +1,13 @@
+//
+//  UdacityClientError.swift
+//  On The Map
+//
+//  Created by Tobias Helmrich on 14.10.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+import Foundation
+
+enum UdacityClientError: String {
+    case wrongCredentials = "Wrong password and/or email"
+}
