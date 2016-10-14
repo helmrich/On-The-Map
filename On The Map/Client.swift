0e329760785a6ca69e50fb2b1d584cebcf0853af
Tobias Helmrich <me@tobias-helmrich.de>
2016-10-14 19:06:43 +0200

    Refactor common client GET code into a more general taskForGET method

--- On The Map/Client.swift
+++ On The Map/Client.swift
@@ -10,13 +10,62 @@ import Foundation
 
 class Client {
     
-    static func convertDataWithCompletionHandler(data: Data, completionHandler: (_ result: Any?, _ error: Error?) -> Void) {
+    // MARK: - Properties
+    
+    let session = URLSession.shared
+    
+    // sharedInstance singleton
+    static let sharedInstance = Client()
+    
+    fileprivate init() {}
+    
+    
+    // MARK: - Functions
+    
+    func taskForGET(withUrl url: URL, headerFields: [String:String]?, completionHandlerForGET: @escaping (_ data: Data?, _ errorMessage: String?) -> Void) {
+        // Create a mutable request and set its properties
+        let request = NSMutableURLRequest(url: url)
+        if let headerFields = headerFields {
+            for (headerFieldName, headerFieldValue) in headerFields {
+                request.addValue(headerFieldValue, forHTTPHeaderField: headerFieldName)
+            }
+        }
+        
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            
+            // Check if there was an error
+            guard error == nil else {
+                completionHandlerForGET(nil, error!.localizedDescription)
+                return
+            }
+            
+            // Check if the status code indicates that the response was successful
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                statusCode >= 200 && statusCode <= 299 else {
+                    completionHandlerForGET(nil, "Unsuccessful response. Status code: \((response as? HTTPURLResponse)?.statusCode)")
+                    return
+            }
+            
+            guard let data = data else {
+                completionHandlerForGET(nil, "Didn't receive data.")
+                return
+            }
+            
+            completionHandlerForGET(data, nil)
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+    static func convertDataWithCompletionHandler(data: Data, completionHandler: (_ result: Any?, _ errorMessage: String?) -> Void) {
         
         var result: Any
         do {
             result = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
         } catch {
-            completionHandler(nil, ClientError.serializationError("Couldn't deserialize data into a usable object."))
+            completionHandler(nil, "Couldn't deserialize data into a usable object.")
             return
         }
         
