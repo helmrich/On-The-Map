274fa06dc3336f347bf9ccec401273e8825b4e6c
Tobias Helmrich <me@tobias-helmrich.de>
2016-10-14 20:33:30 +0200

    Add data source class

---
+++ On The Map/StudentLocationDataSource.swift
@@ -0,0 +1,39 @@
+//
+//  StudentLocationDataSource.swift
+//  On The Map
+//
+//  Created by Tobias Helmrich on 09.10.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+import Foundation
+
+class StudentLocationDataSource {
+    
+    // MARK: - Properties
+    var studentLocations: [StudentLocation]?
+    static let sharedInstance = StudentLocationDataSource()
+    
+    fileprivate init() {}
+
+    
+    // MARK: - Functions
+    
+    func getStudentLocationsForDataSource(limit: Int?, skip: Int?, orderBy: String?, completionHandlerForStudentLocations: @escaping (_ success: Bool, _ errorMessage: String?) -> Void) {
+        ParseClient.sharedInstance.getStudentLocations(limit: limit, skip: skip, orderBy: orderBy) { (studentLocations, errorMessage) in
+            guard errorMessage == nil else {
+                completionHandlerForStudentLocations(false, errorMessage!)
+                return
+            }
+            
+            guard let studentLocations = studentLocations else {
+                completionHandlerForStudentLocations(false, "No student locations found.")
+                return
+            }
+            
+            self.studentLocations = studentLocations
+            completionHandlerForStudentLocations(true, nil)
+            
+        }
+    }
+}
